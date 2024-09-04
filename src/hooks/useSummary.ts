84137a54af9a15f8b160da875d7bcb2626132d66
Diego Veiga <sdiegoveiga@gmail.com>
2024-09-04 20:46:18 -0300

    Creating our own hook: summary

---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,23 @@
+import { useContext, useMemo } from 'react'
+import { TransactionContext } from '../contexts/TransactionsContext'
+
+export function useSummary() {
+  const { transactions } = useContext(TransactionContext)
+
+  const summary = useMemo(() => transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.income += transaction.price
+      acc.total += transaction.price
+    } else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+    return acc
+  }, {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  }), [transactions])
+
+  return summary
+}
